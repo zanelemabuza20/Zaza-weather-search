4f92d61778ceffee893d08b175e28ff662b27823
zanelemabuza20 <zanelemabuza20@gmail.com>
2024-04-30 01:09:59 +0200

    Add basic search engine

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function handleSearchSubmit(event) {
+   event.preventDefault();
+   let searchInput = document.querySelector("#search-form-input");
+   console.log(searchInput.value);  
+   let cityElement = document. querySelector("#city");
+   cityElement.innerHTML = searchInput.value;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let searchFormElement = document.querySelector("#search-form");
+    searchFormElement.addEventListener("submit", handleSearchSubmit);
+});
